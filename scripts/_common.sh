6400b9a3e5e1259a904b00944f1a4e360178c00a
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-01-18 09:22:18 +0100

    So does NGINX

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql postgresql-contrib nginx python3-venv libpq-dev"
+pkg_dependencies="postgresql postgresql-contrib python3-venv libpq-dev"
 
 #=================================================
 # PERSONAL HELPERS
