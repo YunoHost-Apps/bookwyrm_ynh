3e7bfebec9719ebb943c472105d5f8676abac458
oufmilo <44617467+oufmilo@users.noreply.github.com>
2023-02-06 10:45:54 +0100

    test

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql postgresql-contrib python3 python3-venv libpq-dev docker-compose"
+pkg_dependencies="postgresql postgresql-contrib python3 python3-venv libpq-dev docker-compose-plugin"
 
 #=================================================
 # PERSONAL HELPERS
